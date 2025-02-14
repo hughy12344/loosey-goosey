ca277ebf05dfe42f15ba017e4b32e5560929afeb
hughy12344 <hugh.cameron102@gmail.com>
2025-02-14 11:23:01 +1100

    Ran JS standard for code editing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import MyCalendar from './Calendar.js'
-import './App.css';
+import './App.css'
 
-function App() {
+function App () {
   return (
     <div>
       <MyCalendar />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Calendar.js
+++ src/Calendar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
@@ -6,31 +6,29 @@ import 'react-big-calendar/lib/css/react-big-calendar.css'
 const localiser = momentLocalizer(moment)
 
 const MyCalendar = () => {
-    const [events, setEvents] = useState([
-        {
-            title: "Sample appointment",
-            start: new Date(2025, 1, 14, 10, 0),
-            end: new Date(2025, 1, 14, 11, 0)
-        }
-    ])
+  const [events, setEvents] = useState([
+    {
+      title: 'Sample appointment',
+      start: new Date(2025, 1, 14, 10, 0),
+      end: new Date(2025, 1, 14, 11, 0)
+    }
+  ])
 
-    
-    
-    return(
+  return (
     <div>
-        <div>
-            <h1>Spinal Health Organiser</h1>
-        </div>
-        <div>
-            <Calendar 
-                localizer={localiser}
-                events={events}
-                style={{height: 500}}
-            />
-        </div>
+      <div>
+        <h1>Spinal Health Organiser</h1>
+      </div>
+      <div>
+        <Calendar
+          localizer={localiser}
+          events={events}
+          style={{ height: 500 }}
+        />
+      </div>
     </div>
 
-    )
+  )
 }
 
 export default MyCalendar
