3c8055101bfdc1e23d62b4f0e1892a9c002ab74b
hughy12344 <hugh.cameron102@gmail.com>
2025-02-18 15:38:58 +1100

    Updated all REST API routes

--- server/routes/appointments.js
+++ server/routes/appointments.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Appointment = require('../models/appointment')
+const appointment = require('../models/appointment')
 
 //Getting all
 router.get('/', async (req, res) => {
@@ -8,13 +9,15 @@ router.get('/', async (req, res) => {
         const appointments = await Appointment.find()
         res.json(appointments)
     } catch (err) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: err.message })
     }
 })
-//Getting one
-router.get('/:id', (req, res) => {
 
+//Getting one
+router.get('/:id', getAppointment, (req, res) => {
+    res.json(res.appointment)
 })
+
 //Creating one
 router.post('/', async (req, res) => {
     const appointment = new Appointment({
@@ -28,12 +31,41 @@ router.post('/', async (req, res) => {
     }
 })
 //Updating one
-router.patch('/:id', (req, res) => {
-    
+router.patch('/:id', getAppointment, async (req, res) => {
+    if (req.body.title != null) {
+        res.appointment.title = req.body.title
+    }
+
+    try {
+        const updatedAppointment = await res.appointment.save()
+        res.json(updatedAppointment)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 })
 //Deleting one
-router.delete('/:id', (req, res) => {
-    
+router.delete('/:id', getAppointment, async (req, res) => {
+    try {
+        await res.appointment.deleteOne()
+        res.json({ message: 'Deleted appointment' })
+    } catch (err) {
+        res.status(500).json({ message: err.message})
+    }
 })
+
+async function getAppointment(req, res, next) {
+    let appointment
+    try{
+        appointment = await Appointment.findById(req.params.id)
+        if (appointment == null) {
+            return res.status(404).json({ message: "Could not find appointment"})
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+
+    res.appointment = appointment
+    next()
+}
 
 module.exports = router
